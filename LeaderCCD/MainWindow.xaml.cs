fdc86eb76f6094c8b8d5042559af03f4a5c2ffe1
zxx <357133157@qq.com>
2022-07-26 22:36:30 +0800

    优化

--- LeaderCCD/MainWindow.xaml.cs
+++ LeaderCCD/MainWindow.xaml.cs
@@ -14,6 +14,8 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Xml;
 using System.Configuration;
+using System.Text.RegularExpressions;
+
 
 
 namespace LeaderCCD
@@ -34,28 +36,76 @@ namespace LeaderCCD
             SpeedBox.Text= ConfigurationManager.AppSettings["SpeedBox"];
             TimesBox1.Text= ConfigurationManager.AppSettings["TimesBox1"];
         }
-       
+        /// <summary>
+        /// 是否是纯数字
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static bool isNumber(string str)
+        {
+            bool isMatch = Regex.IsMatch(str, @"^\d+$"); // 判断字符串是否为数字 的正则表达式
+            return isMatch;
+        }
         private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
         {
             try
             {
-                M.cfa.AppSettings.Settings["TimesBox"].Value = TimesBox.Text;
-                M.cfa.AppSettings.Settings["TimeBox"].Value = TimeBox.Text;
-                M.cfa.AppSettings.Settings["PreBox"].Value = PreBox.Text;
-                M.cfa.AppSettings.Settings["SpeedBox"].Value = SpeedBox.Text;
-                M.cfa.AppSettings.Settings["TimesBox1"].Value = TimesBox1.Text;
-                M.cfa.Save(ConfigurationSaveMode.Modified);
-                ConfigurationManager.RefreshSection("appSettings");
-                int x1 = Convert.ToInt32(TimesBox.Text) & 0xFFFF;
-                int y1= (Convert.ToInt32(TimesBox.Text) >> 16) & 0xFFFF;
-                M.ModbusTCP.ModbusWrite(1, 16, 2002, new int[] {x1,y1 });
-                M.ModbusTCP.ModbusWrite(1, 16, 2102, new int[] { Convert.ToInt32(TimeBox.Text) });
-                M.ModbusTCP.ModbusWrite(1, 16, 501, new int[] { Convert.ToInt32(PreBox.Text) });
-                M.ModbusTCP.ModbusWrite(1, 16, 503, new int[] { Convert.ToInt32(SpeedBox.Text) });
-                int x = Convert.ToInt32(TimesBox1.Text)& 0xFFFF;
-                int y= (Convert.ToInt32(TimesBox1.Text)>>16) & 0xFFFF;
-                M.ModbusTCP.ModbusWrite(1, 16, 2004, new int[] { x,y });
-                MessageBox.Show("设置成功");
+                string mes = "";
+                bool res = true;
+                //if (isNumber(TimesBox.Text)&& (Convert.ToInt32(TimesBox.Text) < 5 || Convert.ToInt32(TimesBox.Text) > 3000))
+                //{
+                //    mes += "气缸动作次数值错误\r\n";
+                //    res = false;
+                //}
+                //if (isNumber(TimeBox.Text) && (Convert.ToInt32(TimeBox.Text) < 5 || Convert.ToInt32(TimeBox.Text) > 3000))
+                //{
+                //    mes += "置位时间值错误\r\n";
+                //    res = false;
+                //}
+                if (!isNumber(PreBox.Text)|| !isNumber(SpeedBox.Text)|| !isNumber(TimesBox1.Text))
+                {
+                    MessageBox.Show("有参数不是数字");
+                    return;
+                }
+                if ( Convert.ToInt32(PreBox.Text) < 5 || Convert.ToInt32(PreBox.Text) > 3000)
+                {
+                    mes += "压力值错误\r\n";
+                    res = false;
+                }
+                if ( Convert.ToInt32(SpeedBox.Text) < 100 || Convert.ToInt32(SpeedBox.Text) > 3000)
+                {
+                    mes += "速度值错误\r\n";
+                    res = false;
+                }
+                if (Convert.ToInt32(TimesBox1.Text) < 1 || Convert.ToInt32(TimesBox1.Text) > 2000000000)
+                {
+                    mes += "次数值错误\r\n";
+                    res = false;
+                }
+                if (res)
+                {
+                    M.cfa.AppSettings.Settings["TimesBox"].Value = TimesBox.Text;
+                    M.cfa.AppSettings.Settings["TimeBox"].Value = TimeBox.Text;
+                    M.cfa.AppSettings.Settings["PreBox"].Value = PreBox.Text;
+                    M.cfa.AppSettings.Settings["SpeedBox"].Value = SpeedBox.Text;
+                    M.cfa.AppSettings.Settings["TimesBox1"].Value = TimesBox1.Text;
+                    M.cfa.Save(ConfigurationSaveMode.Modified);
+                    ConfigurationManager.RefreshSection("appSettings");
+                    int x1 = Convert.ToInt32(TimesBox.Text) & 0xFFFF;
+                    int y1 = (Convert.ToInt32(TimesBox.Text) >> 16) & 0xFFFF;
+                    M.ModbusTCP.ModbusWrite(1, 16, 2002, new int[] { x1, y1 });
+                    M.ModbusTCP.ModbusWrite(1, 16, 2102, new int[] { Convert.ToInt32(TimeBox.Text) });
+                    M.ModbusTCP.ModbusWrite(1, 16, 501, new int[] { Convert.ToInt32(PreBox.Text) });
+                    M.ModbusTCP.ModbusWrite(1, 16, 503, new int[] { Convert.ToInt32(SpeedBox.Text) });
+                    int x = Convert.ToInt32(TimesBox1.Text) & 0xFFFF;
+                    int y = (Convert.ToInt32(TimesBox1.Text) >> 16) & 0xFFFF;
+                    M.ModbusTCP.ModbusWrite(1, 16, 2004, new int[] { x, y });
+                    MessageBox.Show("设置成功");
+                }
+                else
+                {
+                    MessageBox.Show(mes);
+                }
             }
             catch (Exception exception)
             {
--- LeaderCCD/MainWindowViewModel.cs
+++ LeaderCCD/MainWindowViewModel.cs
@@ -33,39 +33,40 @@ namespace LeaderCCD
         {
             DoPLCStatusChanged(e);
         }
-
+        /// <summary>
+        /// 计次，0就是刚开始
+        /// </summary>
         private int times;
+        /// <summary>
+        /// 和max都是最大值，但是这是为了防止保存有时间延迟导致数据不一样
+        /// </summary>
         private double x;
+
+        private bool test;
         private void DoPLCStatusChanged(string e)
         {
             if (e == "Start")
             {
+                test = Convert.ToInt32(ConfigurationManager.AppSettings["TimesBox1"]) < 11;
                 times = 0;
                 max = 0;
                 ModbusTCP.ModbusWrite(1, 15, 350, new int[] { 0 });
                 lineSeries.Points.Clear();
-                
+                ModelRes.InvalidatePlot(true);
             }
 
             if (e == "Position")
             {
                 ModbusTCP.ModbusWrite(1, 15, 351, new int[] { 0 });
                 x = max;
-                if (times == 0)
-                {
-                    Saveinform(true,x.ToString());
-                }
-                else
-                {
-                    Saveinform(false, x.ToString());
-                }
+                Saveinform(times == 0, x.ToString());
                 Getline(times, x);
                 times++;
                 max = 0;
             }
         }
         #region 属性
-       
+
         private bool start;
         public bool Start
         {
@@ -118,13 +119,13 @@ namespace LeaderCCD
                 PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Position"));
             }
         }
-        private string plcstate="PLC未连接";
+        private string plcstate = "PLC未连接";
         public string PLCState
         {
             get
             {
                 return plcstate;
-            } 
+            }
             set
             {
                 plcstate = value;
@@ -142,11 +143,11 @@ namespace LeaderCCD
             set
             {
                 plcipaddress = value;
-                cfa.AppSettings.Settings["PlcIpAddress"].Value =value;
+                cfa.AppSettings.Settings["PlcIpAddress"].Value = value;
                 cfa.Save(ConfigurationSaveMode.Modified);
                 ConfigurationManager.RefreshSection("appSettings");
-                ModbusTCP.RemoteIPAddress=value;
-                if(ModbusTCP.ModbusState)
+                ModbusTCP.RemoteIPAddress = value;
+                if (ModbusTCP.ModbusState)
                     ModbusTCP.Close();
                 ModbusTCP.StartConnect();
                 PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("PlcIpAddress"));
@@ -157,9 +158,9 @@ namespace LeaderCCD
 
         double max;
         static bool mHasStartPLC = false;
-         public  DXH.Modbus.DXHModbusTCP ModbusTCP;
+        public DXH.Modbus.DXHModbusTCP ModbusTCP;
         public static event EventHandler<string> PLCStatusChanged;
-        private  async void StartReadPLC()
+        private async void StartReadPLC()
         {
             if (!mHasStartPLC)
                 mHasStartPLC = true;
@@ -174,14 +175,18 @@ namespace LeaderCCD
                         Thread.Sleep(100);
                         int[] a = ModbusTCP.ModbusRead(1, 3, 88, 2);//读位置
                         int[] b = ModbusTCP.ModbusRead(1, 1, 350, 2);//开始
-                        if (a == null||b==null) continue;
+                        if (a == null || b == null) continue;
                         int p = a[0] + (a[1] << 16);
-                        xyzPosition = Math.Round(Convert.ToDouble(p),2)/100;
+                        xyzPosition = Math.Round(Convert.ToDouble(p), 2) / 100;
+                        //if (t)
+                        //{
+                        //    Saveinform(times == 0, xyzPosition.ToString());
+                        //}
                         if (xyzPosition > max)
                         {
                             max = xyzPosition;
                         }
-                        Start = b[0]==1;
+                        Start = b[0] == 1;
                         Position = b[1] == 1;
                     }
                     catch (Exception e)
@@ -192,47 +197,57 @@ namespace LeaderCCD
             });
         }
 
-        private string oldPath="";
-/// <summary>
-/// 保存数据
-/// </summary>
-/// <param name="n">是否新建</param>
-/// <param name="position">传入位置</param>
-        public void Saveinform(bool n, string position)
+        private string oldPath = "";
+        /// <summary>
+        /// 保存csv
+        /// </summary>
+        /// <param name="n">是不是新开始</param>
+        /// <param name="position">值</param>
+        /// <param name="test">是不是保存测试数据，false保存最大值，true保存实时数据</param>
+        public async void Saveinform(bool n, string position)
         {
-            string filePath="";
-            if (n)
+            await Task.Run((() =>
             {
-                filePath = "E:\\保存参数\\"+ DateTime.Now.ToString("yyyy_MM_dd HH.mm.ss")+".csv";
-                oldPath = filePath;
-            }
-            else
-            {
-                filePath = oldPath;
-            }
-            if (!Directory.Exists("E:\\保存参数"))
-            {
-                Directory.CreateDirectory("E:\\保存参数");
-            }
-            try
-            {
-                if (!File.Exists(filePath))
+                string filePath = "";
+
+                if (n)
                 {
-                    string[] heads = { "时间", "位置" };
-                    Csvfile.AddNewLine(filePath, heads);
+                    if (!test) filePath = "E:\\保存参数\\正常测试" + ConfigurationManager.AppSettings["TimesBox1"] + "次" + DateTime.Now.ToString("yyyy_MM_dd HH.mm.ss") + ".csv";
+                    else
+                    {
+                        filePath = "E:\\保存参数\\实时保存测试" + ConfigurationManager.AppSettings["TimesBox1"] + "次" + DateTime.Now.ToString("yyyy_MM_dd HH.mm.ss") + ".csv";
+                    }
+                    oldPath = filePath;
                 }
-                string[] conte = { System.DateTime.Now.ToString() , position };
-                Csvfile.AddNewLine(filePath, conte);
-            }
-            catch (Exception ex)
-            {
-                LdrLog(ex.Message);
-            }
+                else
+                {
+                    filePath = oldPath;
+                }
+                if (!Directory.Exists("E:\\保存参数"))
+                {
+                    Directory.CreateDirectory("E:\\保存参数");
+                }
+                try
+                {
+                    if (!File.Exists(filePath))
+                    {
+                        string[] heads = { "时间", "位置" };
+                        Csvfile.AddNewLine(filePath, heads);
+                    }
+                    string[] conte = { System.DateTime.Now.ToString(), position };
+                    Csvfile.AddNewLine(filePath, conte);
+                }
+                catch (Exception ex)
+                {
+                    LdrLog(ex.Message);
+                }
+
+            }));
         }
 
         private void ModbusTCP_ConnectStateChanged(object sender, string e)
         {
-            
+
         }
 
         private void ModbusTCP_ModbusStateChanged(object sender, bool e)
@@ -380,7 +395,7 @@ namespace LeaderCCD
             MajorGridlineStyle = LineStyle.Solid,
             MinorGridlineStyle = LineStyle.Dot,
         };
-        private  LineSeries lineSeries = new LineSeries()
+        private LineSeries lineSeries = new LineSeries()
         {
             Title = "Series 1",
             Color = OxyColors.Blue,
@@ -404,7 +419,7 @@ namespace LeaderCCD
         /// <summary>
         /// 实时获取，更新曲线
         /// </summary>
-        public async void Getline(int times,double y)
+        public async void Getline(int times, double y)
         {
             await Task.Run(() =>
             {
@@ -418,7 +433,7 @@ namespace LeaderCCD
         }
 
         #endregion
-        
+
         #region 日志
 
         private string logdata;
